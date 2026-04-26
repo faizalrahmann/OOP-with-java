f20b3b642820e15565db31615a959a779ff05411
isaaal <faizalrahmancontact@gmail.com>
2026-04-26 18:19:21 +0700

    Add bangun datar

---
+++ Abstract_interface_Package/src/bangunDatar/BangunDatar.java
@@ -0,0 +1,16 @@
+package bangunDatar;
+
+public abstract class BangunDatar {
+
+    String warna;
+
+    String getWarna() {
+        return warna;
+    }
+
+    void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    abstract float getLuas();
+}
---
+++ Abstract_interface_Package/src/bangunDatar/MainBangunDatar.java
@@ -0,0 +1,13 @@
+package bangunDatar;
+
+public class MainBangunDatar {
+
+    public static void main(String[] args) {
+
+        BangunDatar segitiga = new SegiTiga(12, 20);
+        BangunDatar lingkaran = new Lingkaran(60);
+
+        System.out.println("Luas dari bangun datar segitiga  : " + segitiga.getLuas());
+        System.out.println("Luas dari bangun datar lingkaran : " + lingkaran.getLuas());
+    }
+}
