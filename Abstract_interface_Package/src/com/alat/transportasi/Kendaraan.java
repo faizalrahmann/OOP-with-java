a5924b593dc23ed719aaa0d5de01ba9a01bc129e
isaaal <faizalrahmancontact@gmail.com>
2026-04-26 18:16:39 +0700

    First Commit

---
+++ Abstract_interface_Package/src/com/alat/transportasi/Kendaraan.java
@@ -0,0 +1,8 @@
+package com.alat.transportasi;
+
+public class Kendaraan {
+
+    public static void main(String[] args) {
+        System.out.println("Belajar package dan interface");
+    }
+}
---
+++ Abstract_interface_Package/src/com/alat/transportasi/beroda/KendaraanBeroda.java
@@ -0,0 +1,8 @@
+package com.alat.transportasi.beroda;
+
+public class KendaraanBeroda {
+
+    public static void main(String[] args) {
+        System.out.println("Ini adalah Kendaraan Beroda");
+    }
+}
