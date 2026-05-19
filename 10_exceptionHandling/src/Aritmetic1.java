cec5a2fa13827dbbd2c460495b7efc57f06eb62e
isaaal <faizalrahmancontact@gmail.com>
2026-05-19 17:10:14 +0700

    fix: add error handling

--- 10_exceptionHandling/src/Aritmetic1.java
+++ 10_exceptionHandling/src/Aritmetic1.java
@@ -1,13 +1,18 @@
 public class Aritmetic1 {
     
     public static void main(String[] args) {
-        
-        int hasil = 10 /0;
+       
+        try {
+            int hasil = 10 /0;
 
         System.out.println(hasil);
 
     } catch (ArithmeticException e) {
+
         System.out.println("tidak bisa dibagi nol");
+     
     }
-    
+
+    System.out.println("program selesai");
+    }
 }
