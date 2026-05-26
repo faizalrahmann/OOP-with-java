dec620fca968ff36cc1bedce3fbfe1755740ac1f
faizalrahmann <faizalrahmancontact@gmail.com>
2026-05-26 16:58:32 +0700

    Menambahkan program buat file

---
+++ 11_fileHandling/src/BuatFile.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class BuatFile {
+    public static void main(String[] args) {
+        try {
+            File f = new File("file.txt");
+            if (f.createNewFile()) {
+                System.out.println("File created: " + f.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            // e.printStackTrace();
+        }
+    }
+}
