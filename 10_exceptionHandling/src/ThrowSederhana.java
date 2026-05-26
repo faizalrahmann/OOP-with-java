1a64631d8a410285373e14765648beb96c304f06
faizalrahmann <faizalrahmancontact@gmail.com>
2026-05-26 16:41:12 +0700

    Error: eror ArithmeticException pada program throw sederhana

---
+++ 10_exceptionHandling/src/ThrowSederhana.java
@@ -0,0 +1,15 @@
+public class ThrowSederhana {
+    
+public static void main(String[] args) {
+     int umur = 15;
+
+     if (umur <17) {
+
+        throw new ArithmeticException("Umur belum cukup");
+
+     }
+
+     System.out.println("Boleh membuat SIM");
+}
+
+}
