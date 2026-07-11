4d525fbed331fd953f1320d90b89d5bc722410d9
faizalrahmann <faizalrahmancontact@gmail.com>
2026-07-11 15:16:13 +0700

    feat: menambah program praktikum file handling

---
+++ 11_fileHandling/src/praktik/FileMerge.java
@@ -0,0 +1,27 @@
+package praktik;
+
+import java.io .*;
+
+public class FileMerge {
+    public static void main(String[] args) {
+        String[] files = {"D:\\file1.txt", "D:\\file2.txt"};
+        String mergedFile = "D:\\merge.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(mergedFile))) {
+            for (String file : files) {
+                try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                    bw.write(line);
+                    bw.newLine();
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+} 
