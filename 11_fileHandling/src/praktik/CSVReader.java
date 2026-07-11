024c5095b1d961d1b9af1841ae25101fb9f14f1c
faizalrahmann <faizalrahmancontact@gmail.com>
2026-07-11 15:21:09 +0700

    feat: chore: ubah CSVReader.java agar menghasilkan jumlah baris students.csv

--- 11_fileHandling/src/praktik/CSVReader.java
+++ 11_fileHandling/src/praktik/CSVReader.java
@@ -2,25 +2,30 @@ package praktik;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io. IOException;
+import java.io.IOException;
 
 public class CSVReader {
     public static void main(String[] args) {
-        String csvFile = "D:\\students.csv";
+        String csvFile = "src/praktik/Students.csv";
         String line;
         String csvSplitBy = ",";
         int indeks = 0;
+
         System.out.println("NIM, NAMA, UMUR, PRODI");
+
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
                 indeks++;
-                if(indeks>1){
+                if (indeks > 1) {
                     String[] student = line.split(csvSplitBy);
                     System.out.println(student[0] + ", " + student[1] + ", " + student[2] + ", " + student[3]);
                 }
             }
+
+            System.out.println("Jumlah baris total: " + indeks);
+            System.out.println("Jumlah data mahasiswa: " + (indeks - 1));
         } catch (IOException e) {
-        e.printStackTrace();
+            e.printStackTrace();
         }
     }
 }
