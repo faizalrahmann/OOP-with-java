5ff47d4985b46b6b9ef1b9867110b4a056042bb3
faizalrahmann <faizalrahmancontact@gmail.com>
2026-07-26 11:35:53 +0700

    Final

---
+++ SistemKlinikSederhana/bin/database/db_klinik.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 26, 2026 at 06:29 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_klinik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+-- Error reading structure for table db_klinik.appointments: #1932 - Table &#039;db_klinik.appointments&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_klinik.appointments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_klinik`.`appointments`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctors`
+--
+-- Error reading structure for table db_klinik.doctors: #1932 - Table &#039;db_klinik.doctors&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_klinik.doctors: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_klinik`.`doctors`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+-- Error reading structure for table db_klinik.patients: #1932 - Table &#039;db_klinik.patients&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_klinik.patients: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_klinik`.`patients`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table db_klinik.users: #1932 - Table &#039;db_klinik.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_klinik.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_klinik`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
